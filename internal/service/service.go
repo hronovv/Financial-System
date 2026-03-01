e5c61d878db554b5e61026b7f00e881a2f2cf431
hronovv <ivanonlane@gmail.com>
2026-03-01 16:17:14 +0300

    smth

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,16 @@
 package service
+
+import (
+	"financial_system/internal/repository"
+)
+
+type Services struct {
+	// Auth AuthService 
+}
+
+
+func NewServices(deps *repository.Repositories) *Services {
+	return &Services{
+		// Auth: NewAuthService(deps.User),
+	}
+}
