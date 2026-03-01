57e0c457f9508e5eea6b3a87993df2cbde1e2701
hronovv <ivanonlane@gmail.com>
2026-03-01 10:01:43 +0300

    smth

---
+++ internal/repository/repository.go
@@ -0,0 +1,15 @@
+package repository
+
+import (
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Repositories struct {
+	// User domain.UserRepository 
+}
+
+func NewRepositories(db *pgxpool.Pool) *Repositories {
+	return &Repositories{
+		// User: postgres.NewUserRepo(db),
+	}
+}
